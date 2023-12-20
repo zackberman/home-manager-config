04b5d86dc488dc34fe75a967f7527f86741d5242
Zack Berman <zackjberman@gmail.com>
2023-12-20 11:18:52 -0500

    packages: add python and ipython

--- packages.nix
+++ packages.nix
@@ -4,19 +4,29 @@
   ...
 }:
 
-{
-  home.packages = [
-    pkgs.bat
-    pkgs.bashInteractive
-    pkgs.delta
-    pkgs.eza
-    pkgs.fzf
-    pkgs.git
-    pkgs.htop
-    pkgs.nix-tree
-    pkgs.openssh
-    pkgs.ripgrep
-    pkgs.tig
-    pkgs.vim
-  ];
-}
+let
+  pypkgs =
+    ps: with ps; [
+      ipython
+    ];
+
+  python = pkgs.python3.withPackages pypkgs;
+
+in
+  {
+    home.packages = [
+      pkgs.bat
+      pkgs.bashInteractive
+      pkgs.delta
+      pkgs.eza
+      pkgs.fzf
+      pkgs.git
+      pkgs.htop
+      pkgs.nix-tree
+      pkgs.openssh
+      pkgs.ripgrep
+      pkgs.tig
+      pkgs.vim
+      python
+    ];
+  }
